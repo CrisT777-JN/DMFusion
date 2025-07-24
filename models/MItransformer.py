3549d6f3f2965cde2f4f9b9188733ef010237f99
CrisT777-JN <151756560+CrisT777-JN@users.noreply.github.com>
2025-07-24 10:50:24 +0800

    Update MItransformer.py

--- models/MItransformer.py
+++ models/MItransformer.py
@@ -11,7 +11,7 @@ import numbers
 from models.FMoEGate import FusedMoEGate,SparseDispatcher,GatingFusionMoEGate
 
 class DepthwiseSeparableConv(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, padding, bias=False): # 通常 DSC 不用偏置
+    def __init__(self, in_channels, out_channels, kernel_size, padding, bias=False): 
         super().__init__()
         self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size,
                                    padding=padding, groups=in_channels, bias=bias)
